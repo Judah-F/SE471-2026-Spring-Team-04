68a87b4c5a8a48d4c69d90ce68287f65204317b7
Blu's Clues Laptop Edition <rubixboy619@gmail.com>
2025-09-17 16:26:10 -0700

    Start Shuffler

---
+++ Code/untitled/src/main/java/org/example/Shuffler.java
@@ -0,0 +1,17 @@
+﻿package org.example;
+
+public class Shuffler {
+    private final int CARD_COUNT = 52;
+    private int[] cards;
+
+    public Shuffler() {
+        cards = new int[CARD_COUNT];
+        for (int i = 0; i < CARD_COUNT; i++) {
+            cards[i] = i;
+        }
+    }
+
+    public int[] GetShuffledArray() {
+        return new int[5];
+    }
+}
