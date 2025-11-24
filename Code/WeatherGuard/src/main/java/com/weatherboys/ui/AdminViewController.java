03b7acf3836157e929448987effe697a2bf9713b
Josh Clemens <josh.clemens@outlook.com>
2025-11-24 11:57:50 -0800

    init JavaFX scenes

---
+++ Code/WeatherGuard/src/main/java/com/weatherboys/ui/AdminViewController.java
@@ -0,0 +1,18 @@
+package com.weatherboys.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class AdminViewController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("AdminViewController initialized");
+
+    }
+
+}
---
+++ Code/WeatherGuard/src/main/java/com/weatherboys/ui/TeacherViewController.java
@@ -0,0 +1,17 @@
+package com.weatherboys.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TeacherViewController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("TeacherViewController initialized");
+
+    }
+
+}
