00cdee7e97b4213d11ef1dbd238e64a884580426
Judah-F <Judahfisher21@gmail.com>
2026-04-25 19:26:20 -0700

    Grace period strat determine status
    Implementing Determine Satus on Grace period Strat

--- Code/WeatherGuard/src/main/java/com/weatherboys/weatherguard/Weather/Strategy/GracePeriodStrategy.java
+++ Code/WeatherGuard/src/main/java/com/weatherboys/weatherguard/Weather/Strategy/GracePeriodStrategy.java
@@ -3,6 +3,7 @@ package com.weatherboys.weatherguard.Weather.Strategy;
 import com.weatherboys.weatherguard.Weather.Weather;
 
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
 
 public class GracePeriodStrategy implements AttendanceRuleStrategyIF {
     public String STATUS_PRESENT="";
@@ -13,9 +14,17 @@ public class GracePeriodStrategy implements AttendanceRuleStrategyIF {
     private int lateWindowMinutes;
 
 
-    public String determineStatus(LocalDateTime checkInTime, LocalDateTime sessionStart, Weather weather){
+    public String determineStatus(LocalDateTime checkInTime, LocalDateTime sessionStart, Weather weather) {
 
-        return "";
+        LocalDateTime Grace = sessionStart.plusMinutes(presentMinutes); //Grace period 
+
+        if (checkInTime.isAfter(Grace)) {
+            return STATUS_LATE;
+        } else if (checkInTime.isBefore(Grace)) {
+            return STATUS_PRESENT;
+        } else {
+            return STATUS_ABSENT;
+        }
     }
     public String name(){
         return "";
