085d2dbafd95d250f8d492223045cb35b412b889
Judah-F <Judahfisher21@gmail.com>
2026-04-25 19:11:30 -0700

    Update StrictAttendanceStrategy.java

--- Code/WeatherGuard/src/main/java/com/weatherboys/weatherguard/Weather/Strategy/StrictAttendanceStrategy.java
+++ Code/WeatherGuard/src/main/java/com/weatherboys/weatherguard/Weather/Strategy/StrictAttendanceStrategy.java
@@ -9,7 +9,16 @@ public class StrictAttendanceStrategy implements AttendanceRuleStrategyIF {
     public String STATUS_ABSENT="";
 
     public String determineStatus(LocalDateTime checkInTime, LocalDateTime sessionStart, Weather weather){
-        return "";
+
+
+        if(checkInTime.isAfter(sessionStart) ){ //if student does not check in precicly when they should have they are late
+            return STATUS_LATE;
+        }
+        else if(checkInTime.equals(sessionStart)){
+            return STATUS_PRESENT;
+        }
+        else
+            return STATUS_ABSENT;
     }
     public String name(){
         return "";
